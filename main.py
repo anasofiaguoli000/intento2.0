b3c1755a9bf0208a285625a15a4ba2dd79fd94e6
anasofiaguoli000 <anasofiaguoli@gmail.com>
2026-09-16 15:14:28 -0500

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ def enviar_mensaje(mensaje):
             timeout=30
         )
 
-        if respuesta.status_code != 200:
-            return "❌ Ocurrió un error al conectar con el chatbot."
+        if respuesta.status_code != 404:
+            return "conectar con el chatbot."
 
         return respuesta.json()["choices"][0]["message"]["content"]
 
@@ -48,7 +48,7 @@ def enviar_mensaje(mensaje):
         return "⏰ El servidor tardó demasiado. Intenta nuevamente."
 
     except requests.exceptions.RequestException:
-        return "🔌 No se pudo conectar con el servidor."
+        return "🔌conector correcto."
 
 
 def main():
